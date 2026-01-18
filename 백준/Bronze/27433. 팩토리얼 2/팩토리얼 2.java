dfe9dc0dbbdb4f9588c146b80de2097d9b808bbd
Hyeonmoon KIM <146147073+hyeonyway@users.noreply.github.com>
2026-01-18 22:47:20 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 120 ms, Memory: 14580 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+
+        br.close();
+        bw.write(factorial(N)+ "\n");
+        bw.flush();
+        bw.close();
+    }
+
+
+    static long factorial(int n){
+        if (n <= 0){
+            return 1;
+        }
+
+        return n*factorial(n-1);
+    }
+}
+        
