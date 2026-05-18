e2b46493d6771dc19d9fc10d84e2e2000864ee62
Hyeonmoon KIM <146147073+hyeonyway@users.noreply.github.com>
2026-05-18 15:01:44 +0900

    [level 2] Title: 타겟 넘버, Time: 2.64 ms, Memory: 60.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,18 @@
+class Solution {
+    static int cnt = 0;
+    public int solution(int[] numbers, int target) {
+        dfs(0, 0, numbers, target);
+        
+        return cnt;
+    }
+    
+    public void dfs(int idx, int sum, int[] numbers, int target) {
+        if(idx == numbers.length - 1) {
+            if(sum + numbers[idx] == target) cnt++;
+            if(sum - numbers[idx] == target) cnt++;
+            return;
+        }
+        dfs(idx + 1, sum + numbers[idx], numbers, target);
+        dfs(idx + 1, sum - numbers[idx], numbers, target);
+    }
+}
