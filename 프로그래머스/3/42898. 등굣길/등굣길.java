1ab4b2ec0ccedaf61ff35dafa6ec987225f71462
Hyeonmoon KIM <146147073+hyeonyway@users.noreply.github.com>
2026-09-06 01:13:08 +0900

    [level 3] Title: 등굣길, Time: 1.62 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,27 +1,40 @@
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
-        int answer = 0;
-        int MOD = 1_000_000_007;
+        int[][] map = new int[n+1][m+1];
         
-        int[][] map = new int[m + 1][n + 1];
         for(int[] puddle: puddles) {
-            map[puddle[0]][puddle[1]] = -1;
+            map[puddle[1]][puddle[0]] = -1;
         }
         
         map[1][1] = 1;
-        for(int i = 1; i <= m; i++) {
-            for(int j = 1; j <= n; j++) {
-                if(i == 1 && j == 1) continue;
-                if(map[i][j] == -1) continue;
-                if(map[i-1][j] == -1) {
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(i == 1 && j == 1) {
+                    continue;
+                }
+                
+                if(map[i][j] == -1) {
+                    continue;
+                }
+                
+                if(i == 1) {
                     map[i][j] = map[i][j-1];
-                } else if(map[i][j-1] == -1) {
+                } else if(j == 1) {
                     map[i][j] = map[i-1][j];
                 } else {
-                    map[i][j] = (map[i-1][j] + map[i][j-1]) % MOD;
+                    if(map[i-1][j] == -1 && map[i][j-1] == -1) {
+                        map[i][j] = 0;
+                    } else if(map[i-1][j] == -1) {
+                        map[i][j] = map[i][j-1];
+                    } else if(map[i][j-1] == -1) {
+                        map[i][j] = map[i-1][j];
+                    } else {
+                        map[i][j] = (map[i][j-1] + map[i-1][j]) % 1000000007;
+                    }
                 }
             }
         }
-        return map[m][n];
+        
+        return map[n][m];
     }
 }
